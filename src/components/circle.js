36423677212c81522bdccfea5ab35b784d82a24a
Dinesh Kanchi <k.d.v.kumar123@gmail.com>
2020-10-24 13:33:39 +0530

    commit

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,20 @@
 
 // declare class
 
-
 // export class using module.exports
+
+import Shape from "./shape";
+export default class Circle extends Shape {
+  constructor(props) {
+    super(props);
+    this.drawShape = this.drawShape.bind(this);
+    this.setState({ color: "green" });
+    this.calculateArea = this.calculateArea.bind(this);
+  }
+  calculateArea() {
+    console.log("circle area");
+  }
+  render() {
+    return <h1>Cirlce</h1>;
+  }
+}
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,23 @@
 // declare class
 
 // export class using module.exports
+import React from "react";
+import Circle from "./circle";
+
+export default class Shape extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "red"
+    };
+  }
+  drawShape() {
+    console.log("shape");
+  }
+  calculateArea() {
+    console.log("area");
+  }
+  render() {
+    return <Circle />;
+  }
+}
