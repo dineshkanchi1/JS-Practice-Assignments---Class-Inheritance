deba3fa56a5d61d67902119cb0cceb5f81c588ff
Dinesh Kanchi <k.d.v.kumar123@gmail.com>
2020-11-10 12:42:49 +0530

    commit

--- src/components/shape.js
+++ src/components/shape.js
@@ -5,7 +5,7 @@ import React from "react";
 
 class Shape extends React.Component {
   constructor() {
-    this.color = "red";
+    this.color = "#ffffff";
   }
   drawShape() {
     console.log("draw");
